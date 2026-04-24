b3001a912ef9edd33f2ba979a70744bee03c5805
root <root@OGSurface8.>
2026-04-23 23:17:35 -0700

    Update Resume Link April 2026

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate IT Professional, Computer Science Graduate, and a Full Stack Software Developer 🚀 having an experience of building Web,Mobile, and backend applications with Java/ Python/ JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks. This very portfolio is served from an AWS EC2 Ubuntu instance fronted by Nginx, built with React and Node, and delivered over HTTPS with Let’s Encrypt—giving me full-stack control from code to cloud."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1avA-Bwql-SieSt2ZGCh5T8NR2WTtMZXJ/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1KbZFSxJxjQLs3kpk0_uiTnRmV_tL0TQA/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
@@ -180,7 +180,7 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Helpdesk Technician",
+      role: "System Admninistrator I/ Helpdesk Technician",
       company: "Be Structured Technology Group",
       companylogo: require("./assets/images/bstgLogo.png"),
       date: "Oct 2025 – Present",
